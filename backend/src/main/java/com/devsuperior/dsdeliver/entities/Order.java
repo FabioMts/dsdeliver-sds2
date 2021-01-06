20d3f0be73fbf845e32091f0ccf4f0918151ad78
Fabio Matos <fabio.thunder@hotmail.com>
2021-01-06 20:46:21 -0300

    Domain model

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -5,13 +5,25 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_order")
 public class Order implements Serializable {
 	
 	
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String address;
 	private Double latitude;
@@ -19,7 +31,12 @@ public class Order implements Serializable {
 	private Instant moment;
 	private OrderStatus status;
 	
-	private Set<Product> products = new HashSet<>();
+	
+	@ManyToMany
+	@JoinTable(name = "tb_order_product", 
+		joinColumns = @JoinColumn(name = "order_id"),
+		inverseJoinColumns = @JoinColumn(name = "product_id"))
+	private Set< Product> products = new HashSet<>();
 	
 	
 	public Order() {
