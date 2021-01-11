31ec08d9cae77d7756b5d9aa8026960f1d72aa8a
Fabio Matos <fabio.thunder@hotmail.com>
2021-01-11 20:38:40 -0300

    add footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,27 @@
+import './styles.css';
+import {ReactComponent as YoutubeIcon} from './youtube.svg';
+import {ReactComponent as LinkedinIcon} from './linkedin.svg';
+import {ReactComponent as InstagramIcon} from './instagram.svg';
+
+
+
+function Footer() {
+    return (
+    <footer className="main-footer">
+        App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+        <div className="footer-icons">
+        <a href="www.//www.youtube.com/c/DevSuperior" target="_new">
+        <YoutubeIcon/>
+        </a>
+        <a href="https://www.linkedin.com/school/devsuperior/" target="_new">
+        <LinkedinIcon/>
+        </a>
+        <a href="https://www.instagram.com/devsuperior.ig/" target="_new" >
+        <InstagramIcon />
+        </a>
+        </div>
+    </footer>
+    )
+}
+
+export default Footer;
