46d6f8b97f0174f15e5b0ed939ca8654034f8c5c
Fabio Matos <fabio.thunder@hotmail.com>
2021-01-12 00:14:10 -0300

    first part of frontend

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from 'react';
 import './styles.css';
 import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
+import { Product } from './types';
+import { fetchProducts } from '../api';
+
 
 function Orders() {
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch(error => console.log(error))
+    }, []);
+
     return (
    <div className="orders-container">
        <StepsHeader />
+       <ProductsList products={products}/>
    </div>
     )
 }
